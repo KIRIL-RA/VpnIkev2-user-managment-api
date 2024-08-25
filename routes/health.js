d1edd8334c2897cf135a26ce364b44e8f5f7f47a
KIRIL-RA <40370448+KIRIL-RA@users.noreply.github.com>
2024-08-25 15:04:10 +0300

    changed filename

--- routes/health.js
+++ routes/health.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { VerifyToken } = require("./../middlewares/token");
+const { VerifyToken } = require("../middlewares/token");
 require('dotenv').config();
 
 /**
